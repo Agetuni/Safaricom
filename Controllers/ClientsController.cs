378bf832a9debcd7894de86ed7265f8ea850a10c
Aleazar yilma <yilma4060@gmail.com>
2022-06-20 15:53:30 +0300

    Client api controller
    Create, GetbyId, Get all nodes are generated

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Safaricom.Data.Context;
+using Safaricom.Data.Model;
+
+namespace Safaricom.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+        public ClientsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClient()
+        {
+          if (_context.Client == null)
+          {
+              return NotFound();
+          }
+            return await _context.Client.ToListAsync();
+        }
+
+        // GET: api/Clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(long id)
+        {
+          if (_context.Client == null)
+          {
+              return NotFound();
+          }
+            var client = await _context.Client.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+      
+
+        // POST: api/Clients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+          if (_context.Client == null)
+          {
+              return Problem("Entity set 'ApplicationContext.Client'  is null.");
+          }
+            _context.Client.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClient", new { id = client.Id }, client);
+        }
+
+      
+
+        private bool ClientExists(long id)
+        {
+            return (_context.Client?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
